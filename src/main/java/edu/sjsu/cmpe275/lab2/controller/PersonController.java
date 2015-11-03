e01263ff330a6510719eb0894832f47532fbeda3
LipingSun <liping.sun@sjsu.edu>
2015-11-02 16:08:45 -0800

    work version - enable json xml html

--- src/main/java/edu/sjsu/cmpe275/lab2/controller/PersonController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/controller/PersonController.java
@@ -1,20 +1,32 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
 import edu.sjsu.cmpe275.lab2.model.Person;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Created by Liping on 10/30/15.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/person/*")
-@RestController
+@Controller
 public class PersonController {
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Person index() {
-        return new Person("firstname", "lastname", "email");
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=html")
+    public String getPersonHTML(@PathVariable("id") String user_id, ModelMap model) {
+        Person person = new Person("Jan", "Frank", "email@gmail.com");
+        model.addAttribute("first_name", person.getFirstname());
+        model.addAttribute("last_name", person.getLastname());
+        model.addAttribute("email", person.getEmail());
+        return "person";
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=xml")
+    @ResponseBody
+    public Person getPersonXML(@PathVariable("id") String user_id) {
+        return new Person("Jan", "Frank", "email@gmail.com");
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Person getPersonJSON(@PathVariable("id") String user_id) {
+        return new Person("Jan", "Frank", "email@gmail.com");
     }
 }
