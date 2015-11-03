9e293810c1e92115a0b46027662bca9d00bafc1a
LipingSun <liping.sun@sjsu.edu>
2015-11-02 21:31:02 -0800

    add error handler

--- src/main/java/edu/sjsu/cmpe275/lab2/controller/PersonController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/controller/PersonController.java
@@ -1,6 +1,8 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
 import edu.sjsu.cmpe275.lab2.model.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -9,24 +11,45 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PersonController {
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=html")
-    public String getPersonHTML(@PathVariable("id") String user_id, ModelMap model) {
-        Person person = new Person("Jan", "Frank", "email@gmail.com");
-        model.addAttribute("first_name", person.getFirstname());
-        model.addAttribute("last_name", person.getLastname());
-        model.addAttribute("email", person.getEmail());
-        return "person";
-    }
-
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=xml")
-    @ResponseBody
-    public Person getPersonXML(@PathVariable("id") String user_id) {
-        return new Person("Jan", "Frank", "email@gmail.com");
-    }
+    //@RequestMapping(value = "{id}", method = RequestMethod.POST)
+    //public createPerson() {
+    //
+    //}
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Person getPersonJSON(@PathVariable("id") String user_id) {
-        return new Person("Jan", "Frank", "email@gmail.com");
+    public ResponseEntity<?> getPersonJSON(@PathVariable("id") String userId) {
+        try {
+            Person person = new Person("Jan", "Frank", "email@gmail.com");
+            return new ResponseEntity<>(person, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=xml", produces = "application/xml")
+    @ResponseBody
+    public ResponseEntity<?> getPersonXML(@PathVariable("id") String userId) {
+        try {
+            Person person = new Person("Jan", "Frank", "email@gmail.com");
+            return new ResponseEntity<>(person, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=html")
+    public String getPersonHTML(@PathVariable("id") String user_id, ModelMap model) {
+        try {
+            Person person = new Person("Jan", "Frank", "email@gmail.com");
+            model.addAttribute("firstName", person.getFirstname());
+            model.addAttribute("lastName", person.getLastname());
+            model.addAttribute("email", person.getEmail());
+            return "person";
+        } catch (Exception e) {
+            model.addAttribute("errorCode", 404);
+            model.addAttribute("errorMessage", e.toString());
+            return "error";
+        }
     }
 }
