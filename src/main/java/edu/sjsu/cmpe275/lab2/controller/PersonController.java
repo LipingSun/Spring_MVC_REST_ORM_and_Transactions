99997a483d86887c663e58103765087af8f97708
LipingSun <liping.sun@sjsu.edu>
2015-11-02 23:19:35 -0800

    CURD draft for Person

--- src/main/java/edu/sjsu/cmpe275/lab2/controller/PersonController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/controller/PersonController.java
@@ -1,22 +1,53 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
+import edu.sjsu.cmpe275.lab2.model.Address;
+import edu.sjsu.cmpe275.lab2.model.Organization;
 import edu.sjsu.cmpe275.lab2.model.Person;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/person/*")
+import java.util.Map;
+
+@RequestMapping("/person*")
 @Controller
 public class PersonController {
 
-    //@RequestMapping(value = "{id}", method = RequestMethod.POST)
-    //public createPerson() {
-    //
-    //}
+    /* -------------------------------------------- Create a person -------------------------------------------- */
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<?> createPerson(@RequestParam("firstname") String firstName,
+                                          @RequestParam("lastname") String lastName,
+                                          @RequestParam("email") String email,
+                                          @RequestParam Map<String,String> params) {
+        Person person = new Person(firstName, lastName, email);
+        person.setDescription(params.get("description"));
+        if (params.containsKey("friends")) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        if (params.containsKey("street") && params.containsKey("city") &&
+                params.containsKey("state") && params.containsKey("zip")) {
+            Address address = new Address(params.get("street"), params.get("city"),
+                    params.get("state"), params.get("zip"));
+            person.setAddress(address);
+        }
+        if (params.containsKey("organization")) {
+            //TODO get orgnization by id
+            Organization org = new Organization("test", "test", new Address("test", "test", "test", "test"));
+            person.setOrganization(org);
+        }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
+
+        //TODO create a person
+
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    /* ---------------------------------------------- Get a person ---------------------------------------------- */
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<?> getPersonJSON(@PathVariable("id") String userId) {
         try {
@@ -27,7 +58,7 @@ public class PersonController {
         }
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=xml", produces = "application/xml")
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=xml", produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
     public ResponseEntity<?> getPersonXML(@PathVariable("id") String userId) {
         try {
@@ -39,7 +70,7 @@ public class PersonController {
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET, params = "format=html")
-    public String getPersonHTML(@PathVariable("id") String user_id, ModelMap model) {
+    public String getPersonHTML(@PathVariable("id") String userId, ModelMap model) {
         try {
             Person person = new Person("Jan", "Frank", "email@gmail.com");
             model.addAttribute("firstName", person.getFirstname());
@@ -50,6 +81,49 @@ public class PersonController {
             model.addAttribute("errorCode", 404);
             model.addAttribute("errorMessage", e.toString());
             return "error";
+        }
+    }
+
+    /* -------------------------------------------- Update a person -------------------------------------------- */
+    @RequestMapping(value = "{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<?> updatePerson(@PathVariable("id") String userId,
+                                          @RequestParam("firstname") String firstName,
+                                          @RequestParam("lastname") String lastName,
+                                          @RequestParam("email") String email,
+                                          @RequestParam Map<String,String> params) {
+        Person person = new Person(firstName, lastName, email);
+        person.setDescription(params.get("description"));
+        if (params.containsKey("friends")) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        if (params.containsKey("street") && params.containsKey("city") &&
+                params.containsKey("state") && params.containsKey("zip")) {
+            Address address = new Address(params.get("street"), params.get("city"),
+                    params.get("state"), params.get("zip"));
+            person.setAddress(address);
+        }
+        if (params.containsKey("organization")) {
+            //TODO get orgnization by id
+            Organization org = new Organization("test", "test", new Address("test", "test", "test", "test"));
+            person.setOrganization(org);
+        }
+
+
+        //TODO update a person
+
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    /* ---------------------------------------------- Delete a person ---------------------------------------------- */
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity<?> deletePerson(@PathVariable("id") String userId) {
+        try {
+            // TODO delete a person by id
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 }
