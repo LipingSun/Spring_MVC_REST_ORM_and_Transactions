92b6dec277e0ab8228a25b718271c354fd6003a3
LipingSun <liping.sun@sjsu.edu>
2015-11-07 14:30:56 -0800

    reformat codes

--- src/main/java/edu/sjsu/cmpe275/lab2/dao/FriendshipDao.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/dao/FriendshipDao.java
@@ -1,7 +1,7 @@
 package edu.sjsu.cmpe275.lab2.dao;
 
 public interface FriendshipDao {
-    public void create(long userId1, long userId2);
+    void create(long userId1, long userId2);
 
-    public void delete(long userId1, long userId2);
+    void delete(long userId1, long userId2);
 }
