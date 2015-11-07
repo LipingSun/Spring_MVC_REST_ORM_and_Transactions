c7c7b211aea6348601e9ab0e3b5f808a1c2be326
LipingSun <liping.sun@sjsu.edu>
2015-11-07 14:35:42 -0800

    reformat codes

--- src/main/java/edu/sjsu/cmpe275/lab2/domain/Person.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/domain/Person.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
